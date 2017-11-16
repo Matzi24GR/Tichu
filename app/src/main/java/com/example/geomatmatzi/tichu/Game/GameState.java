4f5a4815a4eecc1ff09e6155421b8c06dcb090cb
Geomat Matzi <ggeomat@gmail.com>
2017-11-16 18:59:54 +0200

    kiala arxeia pou telika xreiazontai diorthomena sxetika

---
+++ app/src/main/java/com/example/geomatmatzi/tichu/Game/GameState.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.geomatmatzi.tichu.Game;
+
+/**
+ *
+ * @author Win7User
+ */
+    
+    public enum GameState{ UNDEFINED,STARTED,WAIT4GRANT, WAIT4WISH, NEWROUND,PLAYING, ENDROUND, GAMEFINISHED}
+    
