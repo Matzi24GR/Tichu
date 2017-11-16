153e3b97ceab0d6bb41b6587aee936472f2fc555
Geomat Matzi <ggeomat@gmail.com>
2017-11-16 19:34:14 +0200

    diorthosi

--- app/src/main/java/com/example/geomatmatzi/tichu/MainActivity.java
+++ app/src/main/java/com/example/geomatmatzi/tichu/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        final Button button = (Button) findViewById(R.id.button);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -19,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    TextView textView = (TextView) findViewById(R.id.text_view);
-    Button button = (Button) findViewById(R.id.button);
 
 
 }
