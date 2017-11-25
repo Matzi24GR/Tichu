c8faa145af91172005ab1d1fdfafa2c7cba37830
Geomat Matzi <ggeomat@gmail.com>
2017-11-25 19:04:23 +0200

    prosthesi arxeiou kartas

---
+++ app/src/main/java/com/example/geomatmatzi/tichu/TrueGame/Card.java
@@ -0,0 +1,24 @@
+package com.example.geomatmatzi.tichu.TrueGame;
+
+/**
+ * Created by Geomat Matzi on 24/11/2017.
+ */
+
+public class Card {
+    // gia ton upologismo kai sykrisi
+    private int mArithmosKartasInt;
+    private int mXromaKartasInt;
+
+    //gia na emfanizoume ta onomata isos xreiastei
+    private String mArithmosKartas;
+    private String mXromaKartas;
+
+    //gia tis eikones
+    private int mImageId;
+
+    public Card(int ArithmosKartas, int XromaKartas) {
+        mArithmosKartasInt = ArithmosKartas;
+        mXromaKartasInt = XromaKartas;
+
+    }
+}
