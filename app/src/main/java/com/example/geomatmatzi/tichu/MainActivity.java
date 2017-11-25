be8f568313c71825df1589404764504b7f0e13ed
Geomat Matzi <ggeomat@gmail.com>
2017-11-25 19:03:53 +0200

    prosthesi arxeiou kartas

--- app/src/main/java/com/example/geomatmatzi/tichu/MainActivity.java
+++ app/src/main/java/com/example/geomatmatzi/tichu/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.geomatmatzi.tichu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.text_view);
         final Button button = (Button) findViewById(R.id.button);
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textView.setText("Είπαν πως θα είναι εύκολο");
             }
-        });
-    }
+        }
 
-
-}
